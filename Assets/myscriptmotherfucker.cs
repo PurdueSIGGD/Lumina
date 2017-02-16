7aff8630d40cdd93c4e990f4b32982165b544441
EverettG <egwyn@purdue.edu>
2017-02-16 18:06:38 -0500

    Added in an input controller  code to work on
    hope this works

---
+++ Assets/myscriptmotherfucker.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class myscriptmotherfucker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.GetComponent <Rigidbody>().AddForce(new Vector3(3,3,3));
+	}
+}
