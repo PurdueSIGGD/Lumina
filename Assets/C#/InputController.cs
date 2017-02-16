709fd04e7274a8d005d1d56f8ec9f86ab847194d
EverettG <egwyn@purdue.edu>
2017-02-16 18:45:28 -0500

    The player can move now

--- Assets/C#/InputController.cs
+++ Assets/C#/InputController.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class InputController : MonoBehaviour {
 
+	Rigidbody playerphysics;
+
 	// Use this for initialization
 	void Start () {
-		
+		playerphysics = GetComponentInParent<Rigidbody> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		SetMovement ();
+	}
+
+	/*
+	* Setting the movement and applying the force to the player
+	*/
+	void SetMovement(){
+		playerphysics.AddForce(new Vector3(Input.GetAxis ("Horizontal")*10,0,0));
+		playerphysics.AddForce(new Vector3(0,0,Input.GetAxis ("Vertical")*20));
 	}
 }
