946daa7f4afa98b005ab2a8160b7c06267a44c73
EverettG <egwyn@purdue.edu>
2017-02-16 19:32:41 -0500

    Adding in a Movement Controller
    Hopefully this helps sets stuff up

---
+++ Assets/C#/InputGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour {
+
+	public MovementController playerMovement;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		playerMovement.SetMovement();
+	}
+
+
+
+	/*
+	
+	Gets the potential state changes for a player when they press certain buttons. how this is used
+	in the furture remain to be determined. 
+	*/
+
+	void ButtonStates(){
+		if (Input.GetAxis ("Interact") > 0) {
+			Debug.Log ("Trying to interact");
+		}
+	}
+}
---
+++ Assets/C#/MovementController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour {
+
+	Rigidbody playerPhysics;
+
+	// Use this for initialization
+	void Start () {
+		playerPhysics = GetComponentInParent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	/*
+	* Setting the movement and applying the force to the player
+	*/
+	public void SetMovement(){
+		playerPhysics.AddForce(new Vector3(Input.GetAxis ("Horizontal")*10,0,0));
+		playerPhysics.AddForce(new Vector3(0,0,Input.GetAxis ("Vertical")*20));
+	}
+}
