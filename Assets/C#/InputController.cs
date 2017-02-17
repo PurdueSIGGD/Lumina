4f72a1ef1e7dfe2e59d45ccfa350c84526f23d7b
AndrewGraber <graber15@purdue.lcl>
2017-02-16 19:29:00 -0500

    Worked on jumping
    still doesn't prevent jumping in midair

--- Assets/C#/InputController.cs
+++ Assets/C#/InputController.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class InputController : MonoBehaviour {
 
-	Rigidbody playerphysics;
+	Rigidbody playerPhysics;
+	BoxCollider playerCollider;
 
 	// Use this for initialization
 	void Start () {
-		playerphysics = GetComponentInParent<Rigidbody> ();
+		playerPhysics = GetComponentInParent<Rigidbody> ();
+		playerCollider = GetComponentInParent<BoxCollider> ();
 	}
 	
 	// Update is called once per frame
@@ -20,7 +22,28 @@ public class InputController : MonoBehaviour {
 	* Setting the movement and applying the force to the player
 	*/
 	void SetMovement(){
-		playerphysics.AddForce(new Vector3(Input.GetAxis ("Horizontal")*10,0,0));
-		playerphysics.AddForce(new Vector3(0,0,Input.GetAxis ("Vertical")*20));
+		playerPhysics.AddForce(new Vector3(Input.GetAxis ("Horizontal")*10,0,0));
+		playerPhysics.AddForce(new Vector3(0,0,Input.GetAxis ("Vertical")*20));
+		if (!IsAirborne()) {
+			playerPhysics.AddForce (new Vector3 (0, Input.GetAxis ("Jump") * 20, 0));
+		}
+	}
+
+	bool IsAirborne(){
+		RaycastHit[] hits = Physics.BoxCastAll(this.transform.position-(Vector3.down*playerCollider.bounds.extents.y), playerCollider.bounds.extents, Vector3.down);
+		bool hitValid = false;
+		foreach (RaycastHit hit in hits) {
+			Collider col = hit.collider;
+			print ("Collider is: " + col);
+			if(!col.CompareTag("Player")) {
+				print ("Collider was not player");
+				hitValid = true;
+				break;
+			}
+		}
+		if (hitValid) {
+			return false;
+		}
+		return true;
 	}
 }
