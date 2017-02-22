b58b2a189eac2d4621bb477503b577df44f181dc
Andrew Lonsway <alonswa@purdue.edu>
2017-02-22 01:35:41 -0500

    Formatting fix

--- Assets/C#/DestructibleItem.cs
+++ Assets/C#/DestructibleItem.cs
@@ -10,15 +10,14 @@ public class DestructibleItem : Hittable {
      * Try it out!
      */
      
-    public GameObject gibs;
-
-	public override void Hit(float f, Vector3 direction, DamageType damage) {
+    public GameObject gibs; 	
+    public override void Hit(float f, Vector3 direction, DamageType damage) {
         print("was hit");
         GameObject.Instantiate(gibs, transform.position, transform.rotation);
         GameObject.Destroy(this.gameObject);
     }
-    void Start()
-    {
+    void Start() {
+		
         //this.gameObject.GetComponent<Hittable>().Hit();
     }
 }
