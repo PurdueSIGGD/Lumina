7e9f278a329951f6e19742940551346a1f831c6e
Andrew Lonsway <alonswa@purdue.edu>
2017-02-21 20:51:20 -0500

    Added hittable class
    Also created a new gameobject for you guys to play around with. When it
    is hit, it shatters apart. Try out your own hittable scripts and see how
    it works!

---
+++ Assets/C#/DestructibleItem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructibleItem : Hittable {
+    /**
+     * This is a class I created to help you guys practice out hitting.
+     * If your hit method works, it will destroy items such as this.
+     * All it does right now is fall apart into a fractured block.
+     * Try it out!
+     */
+     
+    public GameObject gibs;
+
+	public override void Hit(float f, Vector3 direction, DamageType damage) {
+        print("was hit");
+        GameObject.Instantiate(gibs, transform.position, transform.rotation);
+        GameObject.Destroy(this.gameObject);
+    }
+    void Start()
+    {
+        //this.gameObject.GetComponent<Hittable>().Hit();
+    }
+}
---
+++ Assets/C#/Hittable.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* To add an attack effect, simply find the Hittable 
+ * class, and if it exists, call the desired hit method.
+ * Example: 
+ *      Hittable hit;
+ *      if (hit = target.GetComponent<Hittable>()) {
+ *              hit.Hit();
+ *      }
+ */
+
+abstract public class Hittable : MonoBehaviour {
+    public enum DamageType { Neutral, Fire, Ice, Electric };
+
+    /* You only have to implement this one function */
+
+    abstract public void Hit(float damage, Vector3 direction, DamageType type);
+
+    /* Overloading takes care of every other instance that could be called */
+
+    public void Hit(float damage) {
+        Hit(damage, Vector3.zero, DamageType.Neutral);
+    }
+
+    public void Hit(float damage, Vector3 direction) {
+        Hit(damage, direction, DamageType.Neutral);
+    }
+
+    public void Hit(float damage, DamageType type) {
+        Hit(damage, Vector3.zero, type);
+    }
+
+    public void Hit(Vector3 direction) {
+        Hit(0, direction, DamageType.Neutral);
+    }
+
+    public void Hit() {
+        Hit(0, Vector3.zero, DamageType.Neutral);
+    }
+
+}
