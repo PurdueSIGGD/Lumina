050f8744556c2fc9c46568f7e3e23c1d98422321
cnguyenm <cnguyenm@purdue.edu>
2017-03-23 18:50:41 -0400

    Start Blob Enemy

---
+++ Assets/C#/EnemyScripts/BlobEnemy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlobEnemy : BaseEnemy {
+
+    void Start()
+    {
+
+    }
+
+
+    public override IEnumerator Attack()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Movement()
+    {
+        throw new NotImplementedException();
+    }
+
+   
+    
+	
+}
