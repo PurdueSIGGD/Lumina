966f9da391776f9142b5d62f00833d3479b94ff7
Chau Minh Nguyen <cnguyenm@purdue.edu>
2017-08-19 13:29:07 -0400

    start crab

---
+++ Assets/C#/EnemyScripts/CrabEnemy.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrabEnemy : PatrolGroundEnemy {
+
+
+    public override IEnumerator Attack()
+    {
+        return null;
+    }
+
+    public override void Movement()
+    {
+        
+    }
+
+    public override void OnDamage(float damage, DamageType type)
+    {
+        
+    }
+
+    public override void OnDeath()
+    {
+        
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	
+}
