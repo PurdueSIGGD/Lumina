13535d22e4886814d82bca3308ebd5f7f4ed5f5b
Andrew Lonsway <alonswa@purdue.edu>
2017-09-17 14:26:24 -0400

    Made build ready

--- Assets/C#/DungeonScripts/DungeonEntranceDoor.cs
+++ Assets/C#/DungeonScripts/DungeonEntranceDoor.cs
@@ -3,7 +3,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
 using UnityEditor;
+#endif
 
 public class DungeonEntranceDoor : Door {
 
