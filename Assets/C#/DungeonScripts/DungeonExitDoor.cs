1b1e706dffa257d0a1fbb799efda6d7cd1dd4f0e
Andrew Lonsway <alonswa@purdue.edu>
2017-10-28 20:25:08 -0400

    Bugfixes #3
    Fixed armor getting huge stats increases when upgraded
    Added a light source to the main menu player
    Fixed opening dungeons far away
    Fixed sprinting aimation being stuck when opening doors
    Made mouse less sensitive, and lowered camera animations

--- Assets/C#/DungeonScripts/DungeonExitDoor.cs
+++ Assets/C#/DungeonScripts/DungeonExitDoor.cs
@@ -14,9 +14,7 @@ public class DungeonExitDoor : Door {
     public override void preSceneSwitch(GameObject player) {
         // Set player camera values, enable movement, etc
         player.SendMessage("ExitDungeon");
-
-        // TODO: save the dungeon state into playerPrefs
-        // TODO: discuss with team for expected behavior
+        
     }
 
     public override void sceneSwitchPrep(GameObject player) {
